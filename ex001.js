94bbb799af2663c93382909461488d497dab7110
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-07-29 14:42:45 -0300

    Update ex001.js

--- ex001.js
+++ ex001.js
@@ -1,1 +1,14 @@
+//  Exemplo de uso de var, let e const em javascript
 
+const externo = "Olá, eu sou uma constante global!";
+
+// Declaração de uma variavel usando var
+function exemplovar(){
+     if (true){
+        var mensagem = " Olá faculdade descomplica, eu sou uma var..."
+     }
+     console.log(mensagem);
+}
+
+// Chamando a função exemplovar
+exemplovar()
